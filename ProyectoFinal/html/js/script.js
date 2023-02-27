28da3a8aeeeada6f9c4f17b0b1f189d1d7968d34
Guretodaku <126123964+Guretodaku@users.noreply.github.com>
2023-02-27 19:50:49 -0300

    Add files via upload

---
+++ ProyectoFinal/html/js/script.js
@@ -0,0 +1,57 @@
+document.getElementById('estudios1').addEventListener('click', function(){
+    document.getElementById('estudios').style.display = 'none';
+ }
+ );
+
+ document.getElementById('estudios2').addEventListener('click', function(){
+    document.getElementById('estudios').style.display = 'block';
+ }
+ );
+
+
+
+ document.getElementById('programas1').addEventListener('click', function(){
+    document.getElementById('programas').style.display = 'none';
+ }
+ );
+
+ document.getElementById('programas2').addEventListener('click', function(){
+    document.getElementById('programas').style.display = 'block';
+ }
+ );
+
+
+
+ document.getElementById('idiomas1').addEventListener('click', function(){
+    document.getElementById('idiomas').style.display = 'none';
+ }
+ );
+
+ document.getElementById('idiomas2').addEventListener('click', function(){
+    document.getElementById('idiomas').style.display = 'block';
+ }
+ );
+
+
+
+ document.getElementById('aptitudes1').addEventListener('click', function(){
+    document.getElementById('aptitudes').style.display = 'none';
+ }
+ );
+
+ document.getElementById('aptitudes2').addEventListener('click', function(){
+    document.getElementById('aptitudes').style.display = 'block';
+ }
+ );
+
+
+
+ document.getElementById('experiencia1').addEventListener('click', function(){
+    document.getElementById('experiencia').style.display = 'none';
+ }
+ );
+
+ document.getElementById('experiencia2').addEventListener('click', function(){
+    document.getElementById('experiencia').style.display = 'block';
+ }
+ );
